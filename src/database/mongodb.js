92d4a4c0aac5bbf6c4c9d3a5993141715fa06cdc
Natanniel <natanniel.alves@outlook.com>
2022-07-04 15:01:09 -0300

    deploy

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-  const uri = "mongodb+srv://cassino:cassino@cluster0.rjggj.mongodb.net/betano?retryWrites=true&w=majority";
-  //const uri = "mongodb://localhost:27017/betano";
+  //const uri = "mongodb+srv://cassino:cassino@cluster0.rjggj.mongodb.net/betano?retryWrites=true&w=majority";
+  const uri = "mongodb://localhost:27017/betano";
  
   try{
     mongoose.connect(
